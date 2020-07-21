5362e2dfb8d229134ac6da6b869dce7278357e87
majedul956 <majedulislam956@gmail.com>
2020-07-21 16:58:36 +0600

    Prepeared for deployment

---
+++ src/pages/project.js
@@ -0,0 +1,47 @@
+import React from "react"
+import Image from "gatsby-image"
+import { FaGithubSquare, FaShareSquare, FaGithub } from "react-icons/fa"
+
+const Project = ({
+  node: { title, projecturl, githuburl, stack, image, category },
+}) => {
+  console.log(title)
+  return (
+    <article className="single-project">
+      <div className="image-wrapper">
+        <Image fluid={image.fluid} className="img" alt="" />
+      </div>
+      <div className="content-wrapper">
+        <a
+          href={projecturl}
+          target="_blank"
+          rel="noreferrer"
+          className="fa-share-square"
+        >
+          <FaShareSquare className="share-icon" />
+        </a>
+        <div className="project-info">
+          <h4>{title}</h4>
+          <div className="stack-items">
+            {stack.map(item => (
+              <p key={item.id}>{item.name}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="single-project-footer">
+        <a href={githuburl} target="blank">
+          <i className="fab fa-github">
+            <FaGithub />
+          </i>
+        </a>
+        <a href="#">About Project</a>
+        <a href="" target="_blank" rel="noreferrer">
+          source code
+        </a>
+      </div>
+    </article>
+  )
+}
+
+export default Project
---
+++ src/pages/projects.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Title from "../components/Title"
+import Layout from "../components/Layout"
+import Project from "./project"
+
+const Projects = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulProjects {
+        edges {
+          node {
+            title
+            projecturl
+            githuburl
+            category
+            stack {
+              id
+              name
+            }
+            image {
+              fluid {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const projects = data.allContentfulProjects.edges
+
+  const [items, setItems] = useState([])
+  const [categories, setCategories] = useState([])
+  const [filters, setFilters] = useState({ category: "all" })
+
+  const getCategories = items => {
+    let tempItems = items.map(item => {
+      return item.node.category
+    })
+    let tempCategories = new Set(tempItems)
+    let categories = Array.from(tempCategories)
+    categories = ["all", ...categories]
+    return categories
+  }
+
+  useEffect(() => {
+    setItems(projects)
+    setCategories(getCategories(projects))
+  }, [])
+
+  const handleItems = e => {
+    let value = e.target.value
+    let tempItems = [...projects]
+    if (value === "all") {
+      setItems(tempItems)
+    } else {
+      let items = tempItems.filter(item => item.node.category === value)
+      setItems(items)
+    }
+    setFilters(value)
+  }
+  console.log(categories)
+  return (
+    <Layout>
+      <section className="section">
+        <Title title="projects" />
+        <div className="project-category">
+          <label>filter by technology</label>
+          <select
+            name="category"
+            id="category"
+            value={filters.category}
+            onChange={handleItems}
+            className="text-capitalize"
+          >
+            {categories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="projects section-center">
+          {items.map((project, index) => {
+            return <Project key={index} {...project} />
+          })}
+        </div>
+      </section>
+    </Layout>
+  )
+}
+export default Projects
