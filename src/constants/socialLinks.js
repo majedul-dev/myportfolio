caf9ff6143f39b0c02f0e34e67d5e5e82e12a1a2
majedul956 <majedulislam956@gmail.com>
2020-07-22 07:01:52 +0600

    About Page Setup

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -11,33 +11,33 @@ const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://web.facebook.com/majedul956/",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com/in/majedul-islam-5240981a0/",
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://dribbble.com/majedul_dev",
   },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
-  },
+  // {
+  //   id: 4,
+  //   icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+  //   url: "https://www.twitter.com",
+  // },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
+    url: "https://twitter.com/mazedul_dev",
   },
 ]
 const links = data.map(link => {
   return (
     <li key={link.id}>
-      <a href={link.url} className="social-link">
+      <a href={link.url} target="blank" className="social-link">
         {link.icon}
       </a>
     </li>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,59 @@
 import React from "react"
-import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Layout from "../components/Layout"
+import Title from "../components/Title"
+import Image from "gatsby-image"
+const About = ({
+  data: {
+    allContentfulAboutMePage: { nodes: about },
+  },
+}) => {
+  const { title, stacks, bio, image } = about[0]
+  console.log(stacks)
 
-const about = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            {documentToReactComponents(bio.json)}
+            <h4 className="text-primary">My Skills</h4>
+            <div className="about-stack">
+              {stacks.map(item => (
+                <p key={item.id}>{item.name}</p>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
     </Layout>
   )
 }
 
-export default about
+export const query = graphql`
+  {
+    allContentfulAboutMePage {
+      nodes {
+        title
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+        bio {
+          bio
+          json
+        }
+        stacks {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+export default About
