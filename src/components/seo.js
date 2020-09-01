a9285ddd534897f156cbd07ee8f2485e6ecb523f
majedul-dev <majedulislam956@gmail.com>
2020-09-01 22:07:09 +0600

    Seo complete bug fixed

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,11 +31,12 @@ function SEO({ description, title }) {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+      {/* twitter cards */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
-      <meta name="twitter:image" content={siteUrl} />
+      <meta name="twitter:image" content={`${siteUrl}`} />
     </Helmet>
   )
 }
