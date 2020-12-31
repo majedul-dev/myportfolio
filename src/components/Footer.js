a8e9f3f98883e84395d3e2257ce29725a5d218f8
majedul-dev <majedulislam956@gmail.com>
2020-12-31 21:07:48 +0600

    Footer updated or github social link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,14 @@ const Footer = () => {
     <footer className="footer">
       <div>
         <SocialLinks styleClass="footer-links"></SocialLinks>
+        <div className="footer-contacts">
+          <p>Email: mazedul.contact@gmail.com</p>
+          <p>Whatsapp: +880 1779888301</p>
+          <p>Phone: +880 1643629820</p>
+        </div>
         <h4>
-          &copy;{new Date().getFullYear()} majedul islam. <span>MZDIV</span> all
-          rights reserved
+          &copy;{new Date().getFullYear()} majedul islam. <span>WebDev</span>{" "}
+          all rights reserved
         </h4>
       </div>
     </footer>
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -2,36 +2,30 @@ import React from "react"
 import {
   FaFacebookSquare,
   FaLinkedin,
-  FaDribbbleSquare,
-  // FaBehanceSquare,
   FaTwitterSquare,
+  FaGithubSquare,
 } from "react-icons/fa"
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://web.facebook.com/majedul956/",
+    url: "https://web.facebook.com/majedul956",
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.linkedin.com/in/majedul-islam-5240981a0/",
+    url: "https://www.linkedin.com/in/majedul-dev",
   },
-  {
-    id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://dribbble.com/majedul_dev",
-  },
-  // {
-  //   id: 4,
-  //   icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-  //   url: "https://www.twitter.com",
-  // },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
     url: "https://twitter.com/mazedul_dev",
+  },
+  {
+    id: 3,
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: "https://github.com/majedul-dev",
   },
 ]
 const links = data.map(link => {
